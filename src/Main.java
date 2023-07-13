e2c584aeb62d2586bd184085b794d97df365d506
First project <ekulakov056@gmail.com>
2023-07-14 00:28:36 +0500

    Сделал четвертое задание

--- src/Main.java
+++ src/Main.java
@@ -65,24 +65,25 @@ public static void main(String[] args) {
     }
 
     public static void task4 () {
-        byte a = 126;
-        short b = 1260;
-        int c = 126000;
-        long d = 12600000;
-        float e = 1.5f;
-        double f = 2.5678;
-        char g = 35672;
-        boolean h = true;
+        byte bottlesTwoMinutes = 16;
+        byte startTimeMinutes = 2;
+        byte oneMinutes = 1;
+        byte twentyMinutes = 20;
+        short oneDayMinutes = 1440;
+        short threeDayMinutes = 4320;
+        int oneMonthMinutes = 43200;
+        int bottles1_Minutes = bottlesTwoMinutes / startTimeMinutes;
+        int bottlesTwentyMinutes = bottles1_Minutes * twentyMinutes;
+        int bottlesOneDay = bottles1_Minutes * oneDayMinutes;
+        int bottlesThreeDay = bottles1_Minutes * threeDayMinutes;
+        int bottlesOneMonth = bottles1_Minutes * oneMonthMinutes;
 
-        System.out.println("Задача 4 - инициализация переменных");
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(f);
-        System.out.println(g);
-        System.out.println(h);
+        System.out.println("Задача 4 - операции с переменными");
+        System.out.println("За 20 минут машина произвела " + bottlesTwentyMinutes + " штук бутылок");
+        System.out.println("За сутки машина произвела " + bottlesOneDay + " штук бутылок");
+        System.out.println("За 3 дня машина произвела " + bottlesThreeDay + " штук бутылок");
+        System.out.println("За 1 месяц машина произвела " + bottlesOneMonth + " штук бутылок");
+        System.out.println();
     }
 
     public static void task5 () {
