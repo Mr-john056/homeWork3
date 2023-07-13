92986dd408567e4298d11ad14b6b71f17ecabaaf
First project <ekulakov056@gmail.com>
2023-07-13 22:57:37 +0500

    Сделал первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,180 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        }
+
+    public static void task1 () {
+        byte a = 126;
+        short b = 1260;
+        int c = 126000;
+        long d = 12600000;
+        float e = 1.5f;
+        double f = 2.5678;
+        char g = 35672;
+        boolean h = true;
+
+        System.out.println("Задача 1 - инициализация переменных");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+
+    public static void task2 () {
+        byte a = 126;
+        short b = 1260;
+        int c = 126000;
+        long d = 12600000;
+        float e = 1.5f;
+        double f = 2.5678;
+        char g = 35672;
+        boolean h = true;
+
+        System.out.println("Задача 2 - инициализация переменных");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+
+    public static void task3 () {
+        byte a = 126;
+        short b = 1260;
+        int c = 126000;
+        long d = 12600000;
+        float e = 1.5f;
+        double f = 2.5678;
+        char g = 35672;
+        boolean h = true;
+
+        System.out.println("Задача 3 - инициализация переменных");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+
+    public static void task4 () {
+        byte a = 126;
+        short b = 1260;
+        int c = 126000;
+        long d = 12600000;
+        float e = 1.5f;
+        double f = 2.5678;
+        char g = 35672;
+        boolean h = true;
+
+        System.out.println("Задача 4 - инициализация переменных");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+
+    public static void task5 () {
+        byte a = 126;
+        short b = 1260;
+        int c = 126000;
+        long d = 12600000;
+        float e = 1.5f;
+        double f = 2.5678;
+        char g = 35672;
+        boolean h = true;
+
+        System.out.println("Задача 1 - инициализация переменных");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+
+    public static void task6 () {
+        byte a = 126;
+        short b = 1260;
+        int c = 126000;
+        long d = 12600000;
+        float e = 1.5f;
+        double f = 2.5678;
+        char g = 35672;
+        boolean h = true;
+
+        System.out.println("Задача 1 - инициализация переменных");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+
+    public static void task7 () {
+        byte a = 126;
+        short b = 1260;
+        int c = 126000;
+        long d = 12600000;
+        float e = 1.5f;
+        double f = 2.5678;
+        char g = 35672;
+        boolean h = true;
+
+        System.out.println("Задача 1 - инициализация переменных");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+
+    public static void task8 () {
+        byte a = 126;
+        short b = 1260;
+        int c = 126000;
+        long d = 12600000;
+        float e = 1.5f;
+        double f = 2.5678;
+        char g = 35672;
+        boolean h = true;
+
+        System.out.println("Задача 1 - инициализация переменных");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
     }
 }
